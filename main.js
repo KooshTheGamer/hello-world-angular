d358be836b59f0193ac0394ce5e1a3f91143223d
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-06-15 10:12:07 -0400

    commiting

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function log(message) {
-    console.log(message);
-}
-var message = 'Hello World';
-log(message);
+// function doSomething() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// // i is declared to the nearest function when decalred as a "var"
+// //when declaring variables, declaring them as a type, will assign it to its block
+// doSomething();
+
+
+
--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-function log(message)
-{
-    console.log(message)
+function doSomething() {
+    for (let i =0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log('Finally: ' + i);
 }
+// i is declared to the nearest function when decalred as a "var"
+//when declaring variables, declaring them as a type, will assign it to its block
+doSomething();
 
-var message = 'Hello World';
 
-log(message);
+
+
