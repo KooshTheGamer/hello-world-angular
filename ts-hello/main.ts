6a8bcfcbcca0a6337424b68b3e5f8ee49ce09c0f
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-06-15 12:48:56 -0400

    Changed File location of test files and created seperate class file

---
+++ ts-hello/main.ts
@@ -0,0 +1,4 @@
+
+let point = new Point(1, 2);
+
+point.draw();
---
+++ ts-hello/point.ts
@@ -0,0 +1,8 @@
+class Point {
+    constructor(private _x?: number, private _y?: number) {
+    }
+    
+    draw() {
+        console.log('X: ' + this._x + ' Y: ' + this._y);
+    } 
+}
