44e1e9bb3f9ee680cab289738cc9054d0fd6ba26
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-06-15 16:32:54 -0400

    tester file

---
+++ ts-hello/tester.ts
@@ -0,0 +1,29 @@
+class Point {
+    constructor(private x?: number, private y?: number) {
+    }
+    
+    draw() {
+        console.log('X: ' + this.x + ' Y: ' + this.y);
+    }
+
+    get X(){
+        return this.x;
+    }
+
+    set X(value){
+        if (value < 0)
+            throw new Error('value cannot be less than 0.');
+
+        this.x = value;
+    }
+
+    getDistance(another: Point) {
+        // ... 
+    }
+}
+
+let point = new Point(1, 2);
+
+let x = point.X
+point.X = 10;
+point.draw();
