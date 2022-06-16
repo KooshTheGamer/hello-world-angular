6f2930c505093adbd01e18f7564b33ad4640ba36
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-06-16 10:55:47 -0400

    gfdch

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,30 +1,5 @@
-var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    Point.prototype.draw = function () {
-        console.log('X: ' + this.x + ' Y: ' + this.y);
-    };
-    Object.defineProperty(Point.prototype, "X", {
-        get: function () {
-            return this.x;
-        },
-        set: function (value) {
-            if (value < 0)
-                throw new Error('value cannot be less than 0.');
-            this.x = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Point.prototype.getDistance = function (another) {
-        // ... 
-    };
-    return Point;
-}());
-var point = new Point(1, 2);
-var x = point.X;
-point.X = 10;
+"use strict";
+exports.__esModule = true;
+var point_1 = require("./point");
+var point = new point_1.Point(1, 2);
 point.draw();
-
